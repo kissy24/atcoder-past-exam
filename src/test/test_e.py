40ef1bc9a8dc139c2fbb0f7830c905a34c545e20
yuhei-kishida <yuhei.kishida@fiertecorp.com>
2022-05-19 18:16:53 +0900

    Added the structure of the test code for the E question

---
+++ src/test/test_e.py
@@ -0,0 +1,36 @@
+from ..e import User, Users, SNS, parse
+
+
+class Test_User:
+    class test_follow:
+        ...
+
+    class test_followed:
+        ...
+
+    class test_is_following:
+        ...
+
+
+class Test_Users:
+    def test_init(self):
+        ...
+
+
+class Test_SNS:
+    class Test_restore:
+        ...
+
+    class Test_follow:
+        ...
+
+    class Test_follow_back:
+        ...
+
+    class Test_follow_follow:
+        ...
+
+
+class Test_parse:
+    def test_parse(self):
+        ...
