747cf7cdd46ecae63dd4ded39fea58198ac941d7
yuhei-kishida <yuhei.kishida@fiertecorp.com>
2022-06-02 16:36:41 +0900

    update b code

--- src/b.py
+++ src/b.py
@@ -4,26 +4,26 @@ from typing import ClassVar, Iterable
 
 @dataclass
 class GradedSequence:
-    """階差数列を生成するクラス"""
+    """階差数列を表現するクラス"""
 
     GREATER: ClassVar[str] = "up"
     LESS: ClassVar[str] = "down"
     EQUAL: ClassVar[str] = "stay"
-    sequence: InitVar[list[int]]
+    _sequence: InitVar[list[int]]
 
-    def __post_init__(self, sequence: list[int]):
-        if len(sequence) < 2:
+    def __post_init__(self, _sequence: list[int]):
+        if len(_sequence) < 2:
             raise ValueError("sequence must have at least 2 elements")
         else:
             self.graded_sequence = list(
-                map(lambda x, y: x - y, sequence[1:], sequence[:-1])
+                map(lambda x, y: x - y, _sequence[1:], _sequence[:-1])
             )
 
     def format(self) -> list[str]:
-        """階差数列を出力用フォーマットに変換する
+        """階差数列を出力用にフォーマットする
 
         Returns:
-            list[str]: 出力用フォーマット
+            list[str]: 増減に合わせてフォーマットされた階差数列
         """
 
         def _format(num: int) -> str:
